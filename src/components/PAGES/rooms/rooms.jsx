60692b3eadfacb5f2d8d02b483eec5a7f0fe4e6d
theodora90 <theodora@lifebank.ng>
2022-09-18 21:38:32 +0100

    rooms completed

--- src/components/PAGES/rooms/rooms.jsx
+++ src/components/PAGES/rooms/rooms.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./rooms.css";
 
 import Container from "../../UI/container/container";
-import one from "../../../assets/images/rooms.jpeg";
+import bookingData from "../../STORE/bookingData.json";
+import Footer from "../../UI/footer/footer";
 
-const Rooms = () => {
+const Rooms = (props) => {
   return (
     <div>
       <div className="room-hero">
@@ -16,39 +17,69 @@ const Rooms = () => {
         </div>
       </div>
       <Container>
-        <div className="row mt-5">
-          <div className="col-lg-6">
-            <div class="card">
-              <img src={one} class="card-img-top" alt="Room" />
-              <div class="card-body">
-                <h5 class="card-title text-center">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
+        <div className="row book-room">
+          {bookingData.map((rooms) => (
+            <div className="col-md-4 mx-auto mt-5" key={rooms.id}>
+              <div className="card border-0 shadow">
+                <img src={rooms.img} className="img-fluid" alt="Room" />
+                <div className="card-body pb-5">
+                  <h5 className="card-title text-center">{rooms.name}</h5>
+                  <p className="card-text">$ {rooms.price}</p>
+                  <div className="text-center">
+                    <button
+                      className="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      Book
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-6">
-            <div class="card">
-              <img src="..." class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
+          ))}
+        </div>
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header border-0 text-center">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Book a Reservation
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close fw-bold text-danger"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form></form>
+              </div>
+              <div className="modal-footer border-0">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
               </div>
             </div>
           </div>
         </div>
       </Container>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/UI/booking-form/form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <div>
+      <form>
+        <h1>Book a Reservation</h1>
+        <input type="text" value="First Name" />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
